24beac1b8d893fef987da7eb59b2deb83c5a689e
adg516 <megabird87@gmail.com>
2023-05-05 16:15:39 -0500

    cleaned some things

--- overUnder.sol
+++ overUnder.sol
@@ -30,17 +30,18 @@ contract OverUnderBetting {
         overUnderLine = _overUnderLine;
     }
 
-    modifier onlyOracle() {
-        require(msg.sender == oracle, "Caller is not the oracle");
+
+    modifier eventNotDone() {
+        require(!eventFinished, "the event is done");
         _;
     }
 
-    modifier eventNotFinished() {
-        require(!eventFinished, "Event has already finished");
+    modifier onlyOwner() {  
+        require(msg.sender == oracle, "you aren't the oracle");
         _;
     }
 
-    function placeBet(bool over) external payable eventNotFinished {
+    function placeBet(bool over) external payable eventNotDone {
         _betIds.increment();
         uint256 betId = _betIds.current();
 
@@ -50,8 +51,8 @@ contract OverUnderBetting {
         emit BetPlaced(betId, msg.sender, msg.value, over);
     }
 
-    function settleBets(uint256 _eventResult) external onlyOracle {
-        require(!eventFinished, "Bets are already settled");
+    function settleBets(uint256 _eventResult) external onlyOwner {
+        require(!eventFinished, "the bets have already been settled");
 
         eventFinished = true;
         eventResult = _eventResult;
@@ -61,20 +62,20 @@ contract OverUnderBetting {
         uint256 totalWinningBets = betAmounts[winningOutcome];
         uint256 totalLosingBets = betAmounts[!winningOutcome];
 
-        for (uint256 i = 1; i <= _betIds.current(); i++) {
+        for (uint256 x = 1; x <= _betIds.current(); x++) {
             Bet storage bet = bets[i];
             if (bet.over == winningOutcome) {
                 uint256 payout = (bet.amount * totalLosingBets) / totalWinningBets;
                 uint256 totalPayout = bet.amount + payout;
                 pendingWithdrawals[bet.bettor] += totalPayout;
-                emit BetSettled(i, bet.bettor, totalPayout);
+                emit BetSettled(x, bet.bettor, totalPayout);
             }
         }
     }
 
     function withdraw() external {
         uint256 amount = pendingWithdrawals[msg.sender];
-        require(amount > 0, "No pending withdrawals");
+        require(amount > 0, "You have nothing to withdraw");
 
         pendingWithdrawals[msg.sender] = 0;
         payable(msg.sender).transfer(amount);
--- parlayBets.sol
+++ parlayBets.sol
@@ -8,7 +8,7 @@ contract ParlayBets {
         bool claimed;
     }
 
-    mapping(uint256 => mapping(address => Bet)) public bets;
+    mapping(uint256 => mapping(address => Bet)) public userBets;
     mapping(uint256 => uint8[]) public actualOutcomes;
 
     event BetPlaced(uint256 eventId, address indexed bettor, uint256 amount, uint8[] outcomes);
@@ -28,7 +28,7 @@ contract ParlayBets {
         require(msg.value > 0, "Must send a positive amount of ETH");
         require(outcomes.length > 1, "Parlay bet must have at least 2 outcomes");
 
-        Bet storage bet = bets[eventId][msg.sender];
+        Bet storage bet = userBets[eventId][msg.sender];
         require(bet.amount == 0, "A bet has already been placed for this event by this bettor");
 
         bet.amount = msg.value;
@@ -46,11 +46,11 @@ contract ParlayBets {
     function claimBet(uint256 eventId) public {
         Bet storage bet = bets[eventId][msg.sender];
         uint8[] storage outcomes = actualOutcomes[eventId];
-        require(!bet.claimed, "Bet already claimed");
-        require(bet.amount > 0, "No bet has been placed for this event by this bettor");
-        require(areEqual(bet.outcomes, outcomes), "Parlay bet outcomes do not match");
+        require(!bet.claimed, "You've already claimed this bet");
+        require(bet.amount > 0, "You didn't claim a parlay for this");
+        require(areEqual(bet.outcomes, outcomes), "Your parlay was wrong");
 
-        uint256 amountToTransfer = bet.amount * 2; // Example payout: double the bet amount
+        uint256 amountToTransfer = bet.amount * 2; //Example payout: double the bet amount
         bet.claimed = true;
         payable(msg.sender).transfer(amountToTransfer);
 
@@ -61,11 +61,13 @@ contract ParlayBets {
         if (a.length != b.length) {
             return false;
         }
+
         for (uint256 i = 0; i < a.length; i++) {
             if (a[i] != b[i]) {
                 return false;
             }
         }
+        
         return true;
     }
 }
